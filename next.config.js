52dff71ebc80220f6a09e602cc8a67095ad9ed63
Damian Ścimborski <kitkutoja@interia.pl>
2023-12-13 22:10:34 +0100

    Update basepath

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    basePath: process.env.NODE_ENV === "production" ? "/website" : "",
+    basePath: process.env.NODE_ENV === "production" ? "/" : "",
 }
 
 module.exports = nextConfig
